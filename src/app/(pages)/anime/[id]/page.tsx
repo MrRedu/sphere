ff49c8b44c72276f2ba8484195015a2c68d159e5
Eduardo <ejrb1234@gmail.com>
2024-12-26 18:09:36 -0400

    Testing API

---
+++ src/app/(pages)/anime/[id]/page.tsx
@@ -0,0 +1,7 @@
+export default function AnimePage() {
+  return (
+    <>
+      <h2>{`</Page>`}</h2>
+    </>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
+'use client'
+import { gql, useQuery } from '@apollo/client'
+
+export const ANIMES = gql`
+  query Page {
+    Page {
+      media {
+        title {
+          english
+          native
+        }
+        siteUrl
+        description
+        id
+      }
+    }
+  }
+`
+
 export default function HomePage() {
+  const { data, loading, error } = useQuery(ANIMES)
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error: {error.message}</p>
+
   return (
     <div>
       <h1 className="text-3xl font-bold underline">Hello World!</h1>
+      {data.Page.media.map((anime: any) => (
+        <p key={anime.id}>{anime.title.english}</p>
+      ))}
     </div>
   )
 }
