34af5e62c84591159dbd26db5d3d389faa43e37f
Eduardo <ejrb1234@gmail.com>
2024-12-26 22:39:40 -0400

    Add: Paquetes y permitir URL para placeholder de imágenes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['via.placeholder.com', 's4.anilist.co', 'placehold.co'],
+  },
 }
 
 export default nextConfig
