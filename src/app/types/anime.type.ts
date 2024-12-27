4750e541a447accbbe8d3f04e5f04348c7098b6c
Eduardo <ejrb1234@gmail.com>
2024-12-27 15:27:11 -0400

    add: API - FullAnime interfaces

--- src/app/types/anime.type.ts
+++ src/app/types/anime.type.ts
@@ -1,4 +1,5 @@
-export interface Anime {
+// Interface to landing-page/sliders/banner, etc -> Simple
+export interface SimpleAnime {
   id: number
   title: {
     english: string
@@ -11,4 +12,103 @@ export interface Anime {
     color: string
   }
   genres: string[]
+}
+
+export interface Media {
+  Media: Anime
+}
+
+export interface Anime {
+  __typename: string
+  id: number
+  title: Title
+  countryOfOrigin: string
+  description: string
+  genres: string[]
+  siteUrl: string
+  format: string
+  type: string
+  bannerImage: string
+  coverImage: CoverImage
+  averageScore: number
+  updatedAt: number
+  startDate: StartEndDate
+  endDate: StartEndDate
+  episodes: number
+  duration: number
+  volumes: null
+  trailer: Trailer
+  characters: Characters
+}
+export interface Title {
+  __typename: string
+  english: string
+  native: string
+}
+
+export interface CoverImage {
+  __typename: string
+  medium: string
+  large: string
+  extraLarge: string
+  color: string
+}
+
+export interface StartEndDate {
+  __typename: string
+  day: number
+  month: number
+  year: number
+}
+
+export interface Trailer {
+  __typename: string
+  id: string
+  site: string
+  thumbnail: string
+}
+export interface Characters {
+  __typename: string
+  nodes: Character[]
+}
+
+export interface Character {
+  __typename: NodeTypename
+  id: number
+  name: Name
+  gender: Gender | null
+  age: null | string
+  isFavourite: boolean
+  image: Image
+  siteUrl: string
+}
+
+export enum NodeTypename {
+  Character = 'Character',
+}
+
+export enum Gender {
+  Female = 'Female',
+  Male = 'Male',
+}
+
+export interface Image {
+  __typename: ImageTypename
+  large: string
+  medium: string
+}
+
+export enum ImageTypename {
+  CharacterImage = 'CharacterImage',
+}
+
+export interface Name {
+  __typename: NameTypename
+  full: string
+  native: string
+  userPreferred: string
+}
+
+export enum NameTypename {
+  CharacterName = 'CharacterName',
 }
